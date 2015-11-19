5d212bfc9ddb5b637db18b95546815d7528dd366
buenonyckolas <buenonyckolas@gmail.com>
2015-11-19 09:13:33 -0200

    TGB
    Trabalho do Grau B

---
+++ AgendaException.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class AgendaException extends IOException{
+	
+	public AgendaException(String message) {
+		super(message);
+	}
+
+}
---
+++ Teste.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Teste {
+
+	public static void main(String[] args) throws IOException {
+	
+		
+		Agenda a = new Agenda();
+		a.carregarAgenda("C:\\bryan@hotmail.com");
+
+	}
+
+}
