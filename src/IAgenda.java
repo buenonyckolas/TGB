aa9127ade6aba2b10414b3d57f21d857a0aab299
Bryan Budelon Dorneles <bryan_dorneles@hotmail.com>
2015-11-19 23:06:26 -0200

    Novos arquivos.

---
+++ src/IAgenda.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public interface IAgenda {
+
+	public void carregarAgenda(String emailUsuario) throws IOException;
+	public void visualizarSemana();
+	public void visualizarDiaSemana(int diaSemana);
+	public void visualizarData(int ano, int mes, int dia);
+	public void novoCompromisso(String descr, int ano, int mes, int dia,
+	int hora, int min, String[] partic) throws AgendaException;
+	public void salvar() throws IOException;
+
+}
